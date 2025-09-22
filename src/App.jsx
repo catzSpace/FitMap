16b11a790c14ff74aa9824cf35ac92c7a27cbda3
catzSpace <o8catzspace8o@gmail.com>
2025-09-22 12:23:41 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Map from "./Map";
+import "leaflet/dist/leaflet.css";
+
+function App() {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
